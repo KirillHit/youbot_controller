9e9a663fa486dda9b2087bda2c2885b91c522067
KirillHit <khitushkink@mail.ru>
2024-09-10 23:38:47 +0300

    add urg_library

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "yaml-cpp/yaml.h"
 #include "youbot_driver/youbot/YouBotBase.hpp"
 #include "youbot_lidar_nav/logger.hpp"
+#include "urg_sensor.h"
+#include "urg_utils.h"
 
 using namespace ybot_ln;
 
